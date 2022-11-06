957c502a65d07dd3a598cb6cb74c08eb67e9c939
TakaoIsTheBest <saheb1379@gmail.com>
2022-11-06 16:41:01 +0330

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home";
+
 function App() {
-	return <div></div>;
+	return (
+		<Router>
+			<div className="app">
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
