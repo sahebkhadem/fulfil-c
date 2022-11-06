5d80ec2ce2df0020edd622494850a6ea44805daf
TakaoIsTheBest <saheb1379@gmail.com>
2022-11-06 16:48:25 +0330

    Style the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import "./App.css";
+
 // Pages
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
 
 function App() {
 	return (
