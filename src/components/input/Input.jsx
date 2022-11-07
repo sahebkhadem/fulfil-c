b8e7b44108c04e92b3cdd002464d0c57e4c03ef5
TakaoIsTheBest <saheb1379@gmail.com>
2022-11-08 00:35:30 +0330

    Add password input with show password button

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
-import { FaExclamationCircle } from "react-icons/fa";
+import { FaExclamationCircle, FaEye, FaEyeSlash } from "react-icons/fa";
 
 // CSS
 import "./Input.css";
 
+// Text input
 export function TextInput({ name = "text", changeHandler = () => {}, label = "Enter text", errors = [] }) {
 	return (
 		<div className="input-container">
@@ -35,5 +35,63 @@ TextInput.propTypes = {
 	name: PropTypes.string,
 	changeHandler: PropTypes.func,
 	label: PropTypes.string,
+	errors: PropTypes.arrayOf(PropTypes.string)
+};
+
+// Email input
+export function PasswordInput({
+	name = "password",
+	changeHandler = () => {},
+	passwordToggler = () => {},
+	label = "Enter password",
+	showPassword = false,
+	confirm = false,
+	errors = []
+}) {
+	return (
+		<div className="input-container">
+			<input
+				type={showPassword ? "text" : "password"}
+				name={name}
+				id={name}
+				onChange={changeHandler}
+				placeholder="Placeholder"
+				style={{ borderColor: errors.length > 0 && "var(--color-error)" }}
+				className={!confirm ? "pr" : ""}
+			/>
+
+			<label htmlFor={name}>{label}</label>
+
+			{!confirm && (
+				<button
+					type="button"
+					title={showPassword ? "Hide password" : "Show password"}
+					onClick={passwordToggler}
+				>
+					{showPassword ? <FaEyeSlash /> : <FaEye />}
+				</button>
+			)}
+
+			{errors.length > 0 && (
+				<div className="errors-container">
+					{errors.map((error, index) => (
+						<div className="error" key={index}>
+							<FaExclamationCircle />
+							<p>{error}</p>
+						</div>
+					))}
+				</div>
+			)}
+		</div>
+	);
+}
+
+PasswordInput.propTypes = {
+	name: PropTypes.string,
+	changeHandler: PropTypes.func,
+	passwordToggler: PropTypes.func,
+	label: PropTypes.string,
+	showPassword: PropTypes.bool,
+	confirm: PropTypes.bool,
 	errors: PropTypes.arrayOf(PropTypes.string)
 };
